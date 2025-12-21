bbbb2604669f46200e5e18cc41ebf5d8d9245ea7
sylvainpoulet69-lang <sylvainpoulet69@gmail.com>
2025-12-21 17:29:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const KID_SIZES = ["4", "6", "8", "10", "12", "14"];
 
 /* === CONFIG BACKEND APPS SCRIPT === */
 window.APPS_SCRIPT_DEPLOY =
-  "https://script.google.com/macros/s/AKfycbxYvFUm1psmaR_YjAQMK1O5ZMqmyBHT-B7i6oIDcRRJVYRaPXup58xJHyKwvWlGA0ZF/exec";
+  "https://script.google.com/macros/s/AKfycbxUttnYUYNR5O0HpfkTIW1UQlNx10dOt8LXM1J08LauZLvkVxVmIHacreVmneLTlyVBzg/exec";
 
 /* === CONFIG MODE GITHUB PAGES === */
 // URL du catalogue statique généré par GitHub Actions (Sheets → data/catalog.json)
